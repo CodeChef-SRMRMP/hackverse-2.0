ea68ebcf566a302d8da87940d9eb17823d3d8c6c
chanakyha <vetrichanakyha2003@gmail.com>
2023-09-05 23:28:06 +0530

    Our Sponsors Page Done

--- components/OurSponsors.tsx
+++ components/OurSponsors.tsx
@@ -9,51 +9,70 @@ import {
 import Image from "next/image";
 
 export const OurSponsors = () => {
+  const titleSponsors = ["xdc.png"];
+
+  const diamondSponsors = [];
+  const goldenSponsors = [];
+
+  const silverSponsors = ["devfolio.png", "solidity-scan.png"];
+  const bronzeSponsors = [
+    "filecoin.png",
+    "polygon.png",
+    "replit.png",
+    "solana.png",
+  ];
+  const poweredBy = ["dimo.png"];
   return (
     <div className="w-screen snap-start min-h-[calc(100vh-100px)]">
       <h1 className="text-center text-3xl font-bold py-5">Our Sponsors</h1>
       <div className="max-w-6xl mx-auto grid grid-cols-1 px-5 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
+        <MainCard
+          imageFolder="titleSponsors"
+          title="Title Sponsors"
+          logos={titleSponsors}
+        />
+        <MainCard title="Diamond Sponsors" />
+        <MainCard title="Golden Sponsors" />
+        <MainCard
+          imageFolder="silverSponsors"
+          title="Silver Sponsors"
+          logos={silverSponsors}
+        />
+        <MainCard
+          imageFolder="bronzeSponsors"
+          title="Bronze Sponsors"
+          logos={bronzeSponsors}
+        />
+        <MainCard imageFolder="powered" title="Powered by" logos={poweredBy} />
       </div>
     </div>
   );
 };
 
-const MainCard = () => {
+interface MainCardProps {
+  title: string;
+  logos?: string[];
+  imageFolder?: string;
+}
+
+const MainCard = ({ title, logos, imageFolder }: MainCardProps) => {
   return (
     <Card className="bg-secondary">
       <CardHeader>
-        <CardTitle>Title Sponsors</CardTitle>
+        <CardTitle>{title}</CardTitle>
       </CardHeader>
       <CardDescription className="grid grid-cols-3 items-center justify-center">
-        <Image
-          src="/assets/images/titleSponsors/XDC_LOGO.c59cc8ecc32ad3417d15.png"
-          width={100}
-          height={100}
-          alt="sponsor"
-        />
-        <Image
-          src="/assets/images/titleSponsors/XDC_LOGO.c59cc8ecc32ad3417d15.png"
-          width={100}
-          height={100}
-          alt="sponsor"
-        />
-        <Image
-          src="/assets/images/titleSponsors/XDC_LOGO.c59cc8ecc32ad3417d15.png"
-          width={100}
-          height={100}
-          alt="sponsor"
-        />
-        <Image
-          src="/assets/images/titleSponsors/XDC_LOGO.c59cc8ecc32ad3417d15.png"
-          width={100}
-          height={100}
-          alt="sponsor"
-        />
+        {logos?.map((logo, index) => {
+          return (
+            <Image
+              key={index}
+              src={`/assets/images/${imageFolder}/${logo}`}
+              width={100}
+              height={100}
+              alt="sponsor"
+            />
+          );
+        })}
       </CardDescription>
       <CardFooter></CardFooter>
     </Card>
